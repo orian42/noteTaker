8fd996835d234875b04858e4320b9ba6f6001410
Jason Ryan <164967582+orian42@users.noreply.github.com>
2024-06-25 22:19:09 -0700

    added paths for index and notes html files

--- server.js
+++ server.js
@@ -5,6 +5,14 @@ const PORT = 3001;
 
 const app = express();
 
+app.get ('/', (req,res) =>
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.get ('/notes', (req,res) =>
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
 app.listen (PORT, () =>
     console.log(`App listening at http://localhost:${PORT}.`)
 );
